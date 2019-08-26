fcc850e40b9e167ad24eea703374168be11fb1ab
shaun <shaundavin13@gmail.com>
2019-08-27 00:01:33 +0700

    edit settings to include some boilerplate, create 2 main apps

--- indosat_b2b/setting_components/base.py
+++ indosat_b2b/setting_components/base.py
@@ -108,7 +108,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+APPEND_SLASH = False
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'core.User'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
